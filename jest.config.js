cea767481c5d7a4a88551da71e24d316cb40d505
Harun Sokullu <harunsokullu@gmail.com>
2020-07-24 20:36:01 +0300

    jest sonar processor

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testResultsProcessor: 'jest-sonar-reporter',
   globals: {
     'ts-jest': {
       isolatedModules: true
     }
-  },
+  }
 }
